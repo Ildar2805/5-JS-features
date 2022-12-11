e476d5ec76ef4219c1fa1bf9de5d92a1a4e93811
Ildar <ildar2805@gmail.com>
2022-12-11 19:22:25 +0300

    Done

--- countdown/task.js
+++ countdown/task.js
@@ -1,15 +1,11 @@
 const counter = document.getElementById('timer');
-const date = new Date(null);
-date.setSeconds(counter.textContent); 
-counter.textContent = date.toISOString().slice(11, 19);
 let timer = setInterval(() => {
-    distance = counter.textContent - 1000;
-    alert(+counter.textContent)
-    date.setSeconds(distance);
-    counter.textContent = date.toISOString().slice(11, 19);
-    if (distance <= 0) {
+    counter.textContent = Number(counter.textContent) - 1;
+    if (counter.textContent < 0) {
         clearInterval(timer);
         alert('Вы победили в конкурсе!');
+        counter.textContent = 0;
     }
-}, 1000)
+}, 1000);
+
 
